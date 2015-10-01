494967e097564f132ee3f88c37fe5515b9324f39
nfo <nfo@b1ba2a4d-1ccd-40fb-aadd-171927d0b338>
2015-10-01 19:20:22 +0000

    fixed scope of user settings

--- brunch/andrea/app/template/menu.php
+++ brunch/andrea/app/template/menu.php
@@ -6,7 +6,11 @@
             <?php if (!Helper::isUser()): ?>
                 <a class="btn btn-success col-xs-12 col-sm-12 col-md-12" href="/"><?php echo _('Join us'); ?></a>
             <?php endif; ?>
-            <?php if (Helper::isUser()): ?>
+            <?php if (Helper::isUser()): 
+                if(isset($_SESSION['user_settings']))
+                    $user_settings = json_decode($_SESSION['user_settings']);
+
+                ?>
                 <a class="" href="/"><?php if (isset($user_settings->profile_picture) && $user_settings->profile_picture) { ?>
                         <img class="hidden-xs img-responsive" src="<?php echo ($user_settings->profile_picture) ? Config::get("upload_path") . "$user_settings->profile_picture" : "no-profile.jpg"; ?>">
                     <?php } else { ?>
--- brunch/andrea/init.php
+++ brunch/andrea/init.php
@@ -4,5 +4,3 @@
 // autoloader works, usealy there is no need for that
 include_once("app/lib/AutoLoader.php");
 include_once("routing.php");
-if(isset($_SESSION['user_settings']))
-    $user_settings = json_decode($_SESSION['user_settings']);
