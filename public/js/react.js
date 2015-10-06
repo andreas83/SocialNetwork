4f9f88fc27ae7258f8a49108482cf7de6ef74d46
Andreas <office@moving-bytes.at>
2015-10-06 22:01:33 +0200

    #7 Fixed syntax highlight and #hashtag replacement issue (thx to jakob aka chilimatic)

--- public/js/react.js
+++ public/js/react.js
@@ -390,25 +390,26 @@ var AuthorText = React.createClass({
                 hljs.highlightBlock(nodes[i]);
             }
         }
-
-        nodes = domNode.querySelectorAll('.text>span');
-        if (nodes.length > 0) {
-            for (var i = 0; i < nodes.length; i = i + 1) {
-                if (nodes[i].childNodes.length > 0) {
-                    if (typeof nodes[i].childNodes[0].nodeValue == "string") {
-
-                        $(nodes[i]).html(nodes[i].childNodes[0].nodeValue.replace(/#(\S*)/g, '<a class="hash" href="/hash/$1">#$1</a>'));
-                    }
-                    console.log(nodes[i].childNodes[0].nodeType);
-                }
-                //$(nodes[i].childNodes[0].nodeValue).text().replace(/#(\S*)/g,'<a class="hash" href="/hash/$1">#$1</a>');
-            }
-        }
     },
 
     render: function render() {
 
         var content = this.props.data.text;
+        var re = /(\<code[\]\>[\s\S]*?(?:.*?)<\/code\>*?[\s\S])|(#\S*)/gi;
+
+        var m;
+        var hash;
+        var tmp_content = content;
+        while ((m = re.exec(content)) !== null) {
+            if (m.index === re.lastIndex) {
+                re.lastIndex++;
+            }
+
+            if (typeof m[2] != "undefined") {
+                hash = m[2].replace("#", "");
+                tmp_content = tmp_content.replace(m[2], '<a href="/hash/' + hash + '">#' + hash + '</a>');
+            }
+        }
 
         return React.createElement(
             'div',
@@ -416,7 +417,7 @@ var AuthorText = React.createClass({
             React.createElement(
                 'div',
                 { className: 'text' },
-                React.createElement('span', { dangerouslySetInnerHTML: { __html: content } })
+                React.createElement('span', { dangerouslySetInnerHTML: { __html: tmp_content } })
             ),
             React.createElement(
                 'div',
--- public/js/react.jsx
+++ public/js/react.jsx
@@ -358,7 +358,7 @@
                             <a href={authorlink}>{this.props.author.name}</a>
                     </strong>
                     <br/>
-                    <a href={permalink}>#{this.props.contentID}</a>
+                    <a href={permalink}>#{this.props.contentID}</a> 
                 </div>
                 <div className="right">
                     {editBtn}
@@ -381,37 +381,36 @@
 
             }
         }
-        
-        nodes = domNode.querySelectorAll('.text>span');
-        if (nodes.length > 0) {
-            for (var i = 0; i < nodes.length; i=i+1) {
-                if(nodes[i].childNodes.length>0)
-                {
-                    if(typeof(nodes[i].childNodes[0].nodeValue)=="string")
-                    {
-                        
-                        $(nodes[i]).html(nodes[i].childNodes[0].nodeValue.replace(/#(\S*)/g,'<a class="hash" href="/hash/$1">#$1</a>'));
-                        
-                        
-                    }
-                    console.log(nodes[i].childNodes[0].nodeType);
-                }
-                //$(nodes[i].childNodes[0].nodeValue).text().replace(/#(\S*)/g,'<a class="hash" href="/hash/$1">#$1</a>');
-            }
-        }
-
-         
     },
     
     render: function () {
         
         var content=this.props.data.text;
+        var re = /(\<code[\]\>[\s\S]*?(?:.*?)<\/code\>*?[\s\S])|(#\S*)/gi; 
+        
+        var m;
+        var hash;
+        var tmp_content=content;
+        while ((m = re.exec(content)) !== null) {
+            if (m.index === re.lastIndex) {
+                re.lastIndex++;
+            }
+
+            if(typeof m[2] !="undefined")
+            {
+                hash=m[2].replace("#","");
+                tmp_content=tmp_content.replace(m[2], '<a href="/hash/'+hash+'">#'+hash+'</a>');
+            }
+            
+        }
+
+
         
         
         return (
                 <div>
                     <div className="text" >
-                        <span dangerouslySetInnerHTML = {{__html: content}} />
+                        <span dangerouslySetInnerHTML = {{__html: tmp_content}} />
                     </div>
                     <div className="action">
                         <a className="btn save hide btn-success">Save</a>
