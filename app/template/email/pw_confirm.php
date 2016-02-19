e50669b2b10be2c9924daf035a959181bb9f6505
Andreas Beder <office@moving-bytes.at>
2016-02-19 16:41:40 +0100

    ticket #29 improved pw reset function

--- app/template/email/pw_confirm.php
+++ app/template/email/pw_confirm.php
@@ -1,13 +1,12 @@
-Hello <?php echo $name; ?><br/>, 
+Hello <?php echo $name; ?>,<br/> 
 
 someone is trying to reset your Password at <?php echo Config::get("address"); ?><br/>
 
 Please follow this link if you really like to reset your Password:<br/>
 
-<?php
-echo $confirm_url; 
-?>
+<a href="<?php echo $confirm_url; ?>"><?php echo $confirm_url; ?></a>
 
+<br/><br/>
 Best Regards
 
 <br/>
