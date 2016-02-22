b500b739222bb0e7b8a2577f575bc7beb355f5e1
lissi <lissi@lissi-XPS-L412Z>
2016-02-23 00:21:56 +0100

    test for spam filter prevention

--- app/template/email/report_content.php
+++ app/template/email/report_content.php
@@ -1,8 +1,12 @@
-Please validate following content:
+<html>
+    <body>
+        <p>Please validate following content:</p>
 
-<a href="<?php echo $url; ?>"><?php echo $url; ?></a>
+        <a href="<?php echo $url; ?>"><?php echo $url; ?></a>
 
-<br/>
-Happy Hacking
-<br/>
-Bot
+        <br/>
+        Happy Hacking
+        <br/>
+        Bot
+    </body>
+</html>
