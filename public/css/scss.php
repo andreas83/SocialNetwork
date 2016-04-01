a20b4723706a2de41c56506f8b48a9e968c10d95
lissi <lissi@lissi-XPS-L412Z>
2016-04-01 22:30:51 +0200

    removed in favor of new sass gulp task

--- public/css/scss.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../../vendor/autoload.php";
-use Leafo\ScssPhp\Compiler;
-use Leafo\ScssPhp\Server;
-
-$scss = new Compiler();
-$scss->setFormatter('Leafo\ScssPhp\Formatter\Compressed');
-$server = new Server('scss', null, $scss);
-$server->serve();
