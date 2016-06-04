b16840b282d682158b7bc0ac12c21c3ddd4a8579
lissi <office@moving-bytes.at>
2016-06-05 00:18:51 +0200

    added reconnection ability (inspired by https://gist.github.com/extraordinaire/4135119)

--- app/lib/database/DBPDO.php
+++ app/lib/database/DBPDO.php
@@ -16,7 +16,32 @@ class DBPDO extends DBHandler
     {
 
         try {
-            $this->dbh = new \PDO(
+            $this->connect();
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+    
+    
+    public function __call($name, array $arguments)
+    {
+        try {
+            $this->connection()->query("SHOW STATUS;")->execute();
+        } catch(\PDOException $e) {
+            if($e->getCode() != 'HY000' || !stristr($e->getMessage(), 'server has gone away')) {
+                throw $e;
+            }
+            $this->reconnect();
+        }
+        return call_user_func_array(array($this->connection(), $name), $arguments);
+    }
+    protected function connection()
+    {
+        return $this->dbh instanceof \PDO ? $this->dbh : $this->connect();
+    }
+    public function connect()
+    {
+        $this->dbh = new \PDO(
                 Config::get('db_dsn') . ";dbname=" . Config::get('db_name'),
                 Config::get('db_user'),
                 Config::get('db_pass'),
@@ -24,8 +49,15 @@ class DBPDO extends DBHandler
                     \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
                 ]
             );
-        } catch (\Exception $e) {
-            throw $e;
-        }
+        $this->dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        return $this->dbh;
     }
+    public function reconnect()
+    {
+        $this->dbh = null;
+        return $this->connect();
+    }
+    
+    
+    
 }
