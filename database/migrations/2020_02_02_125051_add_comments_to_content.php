402670cc12eb710c1c5479b77dcf25bb20cc4f0f
Andreas Beder <office@moving-bytes.at>
2020-02-02 18:12:04 +0100

    added missing migration

---
+++ database/migrations/2020_02_02_125051_add_comments_to_content.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCommentsToContent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contents', function (Blueprint $table) {
+          $table->enum('has_comment', ['true', 'false']);
+          $table->enum('is_comment', ['true', 'false']);
+          $table->unsignedBigInteger('parrent_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('content', function (Blueprint $table) {
+            //
+        });
+    }
+}
