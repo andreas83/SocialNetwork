e406d6ef6cb80e5d94b8e01262eac23e7a007115
Andreas Beder <office@moving-bytes.at>
2020-02-09 10:33:58 +0100

    added missing migration and controller files

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Auth;
+class UserController extends Controller
+{
+  function update(Request $request, $id){
+
+    $user = User::find(Auth::user()->id);
+
+    if($request->has("password")  && !empty($request->password))
+    {
+      $user->password=$request->password;
+    }
+    if($request->email!=$user->email && !empty($request->email))
+    {
+      $user->email=$request->email;
+    }
+    if($request->has("avatar") && !empty($request->avatar) )
+    {
+      $user->avatar=$request->avatar;
+    }
+    if($request->has("bio") && !empty($request->bio) )
+    {
+      $user->bio=$request->bio;
+    }
+
+
+
+    $user->save();
+
+
+
+    return response()->json([
+         'user' => $user,
+     ]);
+  }
+}
---
+++ database/migrations/2020_02_08_191145_add_bio_to_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddBioToUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('bio');
+            $table->string('avatar')->default('user.jpg');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            //
+        });
+    }
+}
