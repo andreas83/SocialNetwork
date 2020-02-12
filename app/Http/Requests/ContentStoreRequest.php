44b6288d8501d7c34766373ab2423670d8b56bc7
Andreas Beder <office@moving-bytes.at>
2020-02-12 19:07:36 +0100

    added missing files

---
+++ app/Http/Requests/ContentStoreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Auth;
+
+class ContentStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+        'html_content' => ['required'],
+        'json_content' => ['required'],
+        'has_comment' =>  ['required', 'boolean'],
+        'is_comment' =>  ['required', 'boolean' ],
+        'anonymous' =>  ['required', 'boolean'],
+        'visibility' =>  ['required', 'in:public,private,friends'],
+        'parrent_id' => ['required', 'integer']
+        ];
+    }
+}
---
+++ app/Http/Requests/LikeStoreRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LikeStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+        'key' => ['required', "in:heart,happy,wink,like,devil,coffee,beer,sunglasses,displeased"],
+        'content_id' => ['required', 'integer'],
+        ];
+    }
+}
