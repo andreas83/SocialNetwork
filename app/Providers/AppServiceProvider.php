0f828c575bcd9ecd3205542826ce22b93fc87688
Andreas Beder <office@moving-bytes.at>
2020-02-14 21:35:22 +0100

    added migration for parrent_id typo fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+         Schema::defaultStringLength(191);
     }
 }
---
+++ database/migrations/2020_02_14_201639_fix_typo_in_content.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixTypoInContent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contents', function (Blueprint $table) {
+            //$table->renameColumn('parrent_id', 'parent_id');
+             DB::statement('ALTER TABLE contents CHANGE parrent_id parent_id bigint(20) unsigned');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('content', function (Blueprint $table) {
+            //
+        });
+    }
+}
